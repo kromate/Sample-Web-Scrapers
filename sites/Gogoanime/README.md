3d96912df5fcd52e856da798545587a07aa36828
Anthony Akpan <52849936+kromate@users.noreply.github.com>
2021-11-22 22:43:28 +0100

    update

---
+++ sites/Gogoanime/README.md
@@ -0,0 +1,268 @@
+# GetAnime_API 🍕🤗
+
+This API scrapes the web for anime and returns a variety of results depending on the endpoint called.
+API URL = https://anime-web-scraper.herokuapp.com
+
+# EndPoints
+
+## /letters, query parameter 'name' 🔤🔍
+
+This call returns a list of JSON objects in the format {name: ##, link:##}
+
+Example: https://anime-web-scraper.herokuapp.com/letters/?name=a
+
+Result 
+
+````JSON
+[
+  {
+    "name": "A Channel",
+    "link": "/category/a-channel"
+  },
+  {
+    "name": "A Channel Ova ",
+    "link": "/category/-a-channel-ova-"
+  },
+  {
+    "name": "A Channel Special ",
+    "link": "/category/a-channel-special-"
+  },
+  {
+    "name": "A Day Before Us",
+    "link": "/category/a-day-before-us"
+  },
+  {
+    "name": "A Letter to Momo (Dub)",
+    "link": "/category/a-letter-to-momo-dub"
+  },
+  {
+    "name": "A Little Snow Fairy Sugar",
+    "link": "/category/a-little-snow-fairy-sugar"
+  },
+  {
+    "name": "A Little Snow Fairy Sugar (Dub)",
+    "link": "/category/a-little-snow-fairy-sugar-dub"
+  }
+]
+````
+
+## /genres, query parameter 'none' 🤺🐲🎮
+
+  This call returns a list of JSON objects in the format {name: ##, link: ##}
+  
+  Example: https://anime-web-scraper.herokuapp.com/genres
+  
+  Result
+  
+````JSON
+[
+  {
+    "name": "Action",
+    "link": "/genre/action"
+  },
+  {
+    "name": "Adventure",
+    "link": "/genre/adventure"
+  },
+  {
+    "name": "Cars",
+    "link": "/genre/cars"
+  },
+  {
+    "name": "Comedy",
+    "link": "/genre/comedy"
+  },
+  {
+    "name": "Dementia",
+    "link": "/genre/dementia"
+  },
+  {
+    "name": "Demons",
+    "link": "/genre/demons"
+  }
+]
+````
+
+## /gl, query parameter 'link' 🔎🗡️🛡️
+
+This call returns a list of JSON objects in the format {name: ##, link: ##, image: ##, release: ##}
+
+Example: https://anime-web-scraper.herokuapp.com/gl?link=/genre/comedy
+
+Results
+````JSON
+[
+  {
+    "name": "Soredemo Ayumu wa Yosetekuru",
+    "link": "/category/soredemo-ayumu-wa-yosetekuru",
+    "image": "https://gogocdn.net/cover/soredemo-ayumu-wa-yosetekuru.png",
+    "release": "\n                                                                                    Released: 2022                                                                            "
+  },
+  {
+    "name": "Kakkou no Iinazuke",
+    "link": "/category/kakkou-no-iinazuke",
+    "image": "https://gogocdn.net/cover/kakkou-no-iinazuke.png",
+    "release": "\n                                                                                    Released: 2022                                                                            "
+  },
+  {
+    "name": "Machikado Mazoku 2nd Season",
+    "link": "/category/machikado-mazoku-2nd-season",
+    "image": "https://gogocdn.net/cover/machikado-mazoku-2nd-season.png",
+    "release": "\n                                                                                    Released: 2022                                                                            "
+  }
+]
+````
+
+## /popular, query parameter 'none' 📢📢
+
+This call returns a list of JSON objects in the format {name: ##, link: ##, image: ##, latest: ##, genre: ##}
+
+Example: https://anime-web-scraper.herokuapp.com/popular
+
+Result
+
+````JSON
+[
+  {
+    "name": "Kyuukyoku Shinka shita Full Dive RPG ga Genjitsu yori mo Kusoge Dattara",
+    "link": "/category/kyuukyoku-shinka-shita-full-dive-rpg-ga-genjitsu-yori-mo-kusoge-dattara",
+    "image": "https://cdnimg.xyz/cover/kyuukyoku-shinka-shita-full-dive-rpg-ga-genjitsu-yori-mo-kusoge-dattara.png",
+    "latest": "Episode 9",
+    "genre": " Action, Comedy, Fantasy, Game                                        "
+  },
+  {
+    "name": "Tokyo Revengers",
+    "link": "/category/tokyo-revengers",
+    "image": "https://cdnimg.xyz/cover/tokyo-revengers.png",
+    "latest": "Episode 9",
+    "genre": " Action, Drama, School, Shounen                                        "
+  },
+  {
+    "name": "Zombieland Saga: Revenge",
+    "link": "/category/zombieland-saga-revenge",
+    "image": "https://cdnimg.xyz/cover/zombieland-saga-revenge.png",
+    "latest": "Episode 9",
+    "genre": " Comedy, Music, Supernatural                                        "
+  }
+]
+````
+
+## /recent, query parameter 'page'⌛⏰
+
+This call returns a list of JSON objects in the format {name: ##, link: ##, image: ##. episode: ##}
+
+Example: https://anime-web-scraper.herokuapp.com/recent?page=1
+
+Result
+
+````JSON
+[
+  {
+    "name": "Chibi Revenger",
+    "link": "/chibi-revenger-episode-8",
+    "image": "https://cdnimg.xyz/cover/chibi-revenger.png",
+    "episode": "Episode 8"
+  },
+  {
+    "name": "Bishounen Tanteidan",
+    "link": "/bishounen-tanteidan-episode-9",
+    "image": "https://cdnimg.xyz/cover/bishounen-tanteidan.png",
+    "episode": "Episode 9"
+  },
+  {
+    "name": "Tokyo Revengers",
+    "link": "/tokyo-revengers-episode-9",
+    "image": "https://cdnimg.xyz/cover/tokyo-revengers.png",
+    "episode": "Episode 9"
+  },
+  {
+    "name": "Shadows House",
+    "link": "/shadows-house-episode-9",
+    "image": "https://cdnimg.xyz/cover/shadows-house.png",
+    "episode": "Episode 9"
+  }
+]
+````
+
+## /search, query parameter 'name' 🧐🔎
+  
+  This call returns a JSON object in the format {name: ##, link: ##, image: ##, release: ##}
+  
+  Example: https://anime-web-scraper.herokuapp.com/search?name=world-trigger
+  
+  One of the results returned by the query
+  
+  ````JSON
+  {
+    "name": "World Trigger",
+    "link": "/category/world-trigger",
+    "image": "https://gogocdn.net/images/upload/161762.jpg",
+    "release": "\n                                                                                    Released: 2014                                                                            "
+  }
+  ````
+
+
+## /desc, query parameter 'link' 🎞📜
+  
+  This call returns a JSON object in the format {id: ##, name: ##, type: ##, summary: ##, genre: ##, release: ##, status: ##, otherNames: ##, episodes: [{start: ##, end: ##}]}
+  
+  Example: https://anime-web-scraper.herokuapp.com/desc?link=/category/world-trigger-dub
+  
+  Result
+  
+  ````JSON
+  {
+  "id": "8933",
+  "name": "world-trigger-dub",
+  "type": " \n\t\t\t\t    Fall 2014 Anime\n\t\t\t\t",
+  "summary": " When a gate to another world suddenly opens on Earth, Mikado City is invaded by strange creatures known as \"Neighbors,\" malicious beings impervious to traditional weaponry. In response to their arrival, an organization called the Border Defense Agency has been established to combat the Neighbor menace through special weapons called \"Triggers.\" Even though several years have passed after the gate first opened, Neighbors are still a threat and members of Border remain on guard to ensure the safety of the planet.\n\nDespite this delicate situation, members-in-training, such as Osamu Mikumo, are not permitted to use their Triggers outside of headquarters. But when the mysterious new student in his class is dragged into a forbidden area by bullies, they are attacked by Neighbors, and Osamu has no choice but to do what he believes is right. Much to his surprise, however, the transfer student Yuuma Kuga makes short work of the aliens, revealing that he is a humanoid Neighbor in disguise.\nNoted",
+  "genre": " \n\t\t\t         Action, School, Sci-Fi, Shounen, Supernatural\t\t\t\t",
+  "release": " 2014",
+  "status": " \n                                      Completed\n                                  ",
+  "otherNames": " ワールドトリガー",
+  "episodes": [
+    {
+      "start": "\n                                0",
+      "end": "73\n                            "
+    }
+  ]
+}
+  ````
+
+
+## /episodes, query parameters 'start', 'end', 'id', 'name' 📺
+
+This call returns a JSON object in the format {name: ##, link: ##}
+
+Example: https://anime-web-scraper.herokuapp.com/episodes?start=1&end=2&id=8933&name=world-trigger-dub
+
+Result
+````JSON
+[
+  {
+    "name": "\n\t\t\t\tEP 2\n                \n                DUB\n              ",
+    "link": " /world-trigger-dub-episode-2"
+  },
+  {
+    "name": "\n\t\t\t\tEP 1\n                \n                DUB\n              ",
+    "link": " /world-trigger-dub-episode-1"
+  }
+]
+````
+
+
+## /downloadLink, query parameter 'link' 📽
+
+This call returns a JSON object in the format {name: ##, link: ##}
+
+Example: https://anime-web-scraper.herokuapp.com/downloadLink?link=/world-trigger-dub-episode-2
+
+Result
+
+````JSON
+{
+    "name": "Download\n            (HDP - mp4)",
+    "link": "https://storage.googleapis.com/river-data-311609/WF6EZJFJ9B/22a_1620636671_136382.mp4"
+  }
+````
+
---
+++ sites/Gogoanime/index.js
@@ -0,0 +1,262 @@
+// THIS IS THE CURRENT VERSION OF THE ANIME WEB SCRAPER, THE SCRAPING IS DONE FROM GOGOANIME AND QUITE LIMITED, OPEN INDEX.JS TO FIND THE CURRENT VERSION
+const express = require('express');
+const quest = require('request');
+const cheerio = require('cheerio');
+const cors = require('cors');
+let port = process.env.PORT || 3000
+
+const app = express();
+app.listen(port)
+
+app.use(cors())
+
+//ROUTES
+
+
+//list by Letter for anime endpoint
+// http://localhost:3000/letters/?name=p ================= Exp
+app.get('/letters', (request, response) => {
+  quest(`https://www1.gogoanime.ai/anime-list-${request.query.name}`, (error, _response, html) => {
+  if (!error && _response.statusCode == 200) {
+        const $ = cheerio.load(html);
+        const searchArray = [];
+
+        $('.anime_list_body ul li a').each((i,el) => {
+          const title = $(el).text();
+          const link = $(el).attr('href');
+          searchArray.push({name:title, link:link})
+        });
+        response.set('Access-Control-Allow-Origin', '*');
+        response.send(searchArray)
+      }
+    });
+});
+
+//list by genre for anime endpoint
+// http://localhost:3000/gl/?link=/genre/ecchi ============= exp
+app.get('/gl', (request, response) => {
+  quest(`https://www1.gogoanime.ai/${request.query.link}`, (error, _response, html) => {
+  if (!error && _response.statusCode == 200) {
+        const $ = cheerio.load(html);
+        const searchArray = [];
+
+        $('ul.items li').each((i,el) => {
+          const title = $(el).find('p.name a').text();
+          const link = $(el).find('p.name a').attr('href');
+          const img = $(el).find('.img img').attr('src');
+          const release = $(el).find('p.released').text();
+          searchArray.push({name:title, link:link, image:img , release:release})
+        });
+        response.set('Access-Control-Allow-Origin', '*');
+        response.send(searchArray)
+      }
+    });
+});
+
+//Genre list for anime endpoint ========================================================== WIP
+// http://localhost:3000/genres =====================
+app.get('/genres', (request, response) => {
+  console.log(request.query.page);
+    quest(`https://www1.gogoanime.ai/`, (error, _response, html) => {
+    if (!error && _response.statusCode == 200) {
+          const $ = cheerio.load(html);
+          const searchArray = [];
+
+          $('nav.menu_top ul li ul li a').each((i,el) => {
+            const title = $(el).text();
+            const link = $(el).attr('href');
+            searchArray.push({name:title, link:link })
+          });
+          response.set('Access-Control-Allow-Origin', '*');
+          response.send(searchArray)
+        }
+      });
+});
+
+//Popular for anime endpoint ========================================================== WIP
+// http://localhost:3000/popular/?page=2 =========== EX
+app.get('/popular', (request, response) => {
+  console.log(request.query.page);
+    quest(`https://ajax.gogo-load.com/ajax/page-recent-release-ongoing.html?page=${request.query.page}`, (error, _response, html) => {
+    if (!error && _response.statusCode == 200) {
+          const $ = cheerio.load(html);
+          const searchArray = [];
+
+          $('.added_series_body > ul li').each((i,el) => {
+            const title = $(el).find('a').toArray()[0].attribs.title;
+            const link = $(el).find('a').attr('href');
+            const genre = $(el).find('p.genres').text().split(':')[1];
+            const img = $(el).find('.thumbnail-popular').css('background').split("'")[1]
+            const Numlatest = $(el).find('a').text().trim().split(',').length
+            const last_latest = $(el).find('a').text().trim().split(',')[Numlatest-1]
+            const latest = $(el).find('a').text().trim().split(',')[Numlatest-1].substring(last_latest.length - 9)
+
+
+
+            searchArray.push({name:title, link:link, image:img , latest:latest, genre:genre})
+          });
+          response.set('Access-Control-Allow-Origin', '*');
+          response.send(searchArray)
+        }
+      });
+});
+
+//Recent for anime endpoint ================================================== WIP
+// the link here is for direct download only not details
+// http://localhost:3000/recent/?page=2 ============= EX
+
+app.get('/recent', (request, response) => {
+    quest(`https://ajax.gogo-load.com/ajax/page-recent-release.html?page=${request.query.page}`, (error, _response, html) => {
+    if (!error && _response.statusCode == 200) {
+          const $ = cheerio.load(html);
+          const searchArray = [];
+
+          $('ul.items li').each((i,el) => {
+            const title = $(el).find('p.name a').text();
+            const episode = $(el).find('p.episode').text();
+            const link = $(el).find('p.name a').attr('href');
+            const img = $(el).find('.img img').attr('src');
+            searchArray.push({name:title, link:link, image:img, episode:episode })
+          });
+          response.set('Access-Control-Allow-Origin', '*');
+          response.send(searchArray)
+        }
+      });
+});
+
+
+//search for anime endpoint
+app.get('/search', (request, response) => {
+    quest(`https://www1.gogoanime.ai/search.html?keyword=${request.query.name}`, (error, _response, html) => {
+    if (!error && _response.statusCode == 200) {
+          const $ = cheerio.load(html);
+          const searchArray = [];
+
+          $('ul.items li').each((i,el) => {
+            const title = $(el).find('p.name a').text();
+            const link = $(el).find('p.name a').attr('href');
+            const img = $(el).find('.img img').attr('src');
+            const release = $(el).find('p.released').text();
+            searchArray.push({name:title, link:link, image:img , release:release})
+          });
+          response.set('Access-Control-Allow-Origin', '*');
+          response.send(searchArray)
+        }
+      });
+});
+
+app.use(cors())
+//GET Anime details
+app.get('/desc', (request, response) => {
+    quest(`https://www1.gogoanime.ai${request.query.link}`, (error, _response, html) => {
+    if (!error && _response.statusCode == 200) {
+          const $ = cheerio.load(html);
+          const availableepisodes = [];
+          const infoArray = [];
+            const name= (request.query.link).split('/')[2]
+            const id = $('input#movie_id').attr('value')
+        // infoArray details
+        // [0] = type
+        // [1] = summary
+        // [2] = genre
+        // [3] = release
+        // [4] = status
+        // [5] = other name
+
+          $('.anime_info_body p.type').each((i,el) => {
+            const det = $(el).text().split(':')[1]
+            infoArray.push({i:det})
+          })
+
+          $('ul#episode_page li').each((i,el) => {
+            const start = $(el).text().split('-')[0]
+            const end = $(el).text().split('-')[1]
+            availableepisodes.push({start:start, end:end})
+          })
+
+         let searchArray=   {
+             id:id,
+            name:name,
+            type:infoArray[0].i,
+                summary:infoArray[1].i,
+                genre:infoArray[2].i,
+                release:infoArray[3].i,
+                status:infoArray[4].i,
+                otherNames:infoArray[5].i,
+                 episodes:availableepisodes}
+
+                 response.set('Access-Control-Allow-Origin', '*');
+          response.send(searchArray)
+        }
+      });
+});
+
+app.use(cors())
+//get Episode for anime endpoint
+app.get('/episodes', (request, response) => {
+    // get the episodes in the anime by parsing all links that are videos
+    quest(`https://ajax.gogocdn.net/ajax/load-list-episode?ep_start=${request.query.start}&ep_end=${request.query.end}&id=${request.query.id}&default_ep=0&alias=${request.query.name}`, (error, _response, html) => {
+        if (!error && _response.statusCode == 200) {
+          const $ = cheerio.load(html);
+          const episodeArr = [];
+          $('a').each((i,el) => {
+                const title = $(el).text()
+                const link = $(el).attr('href');
+                episodeArr.push({name:title, link:link})
+
+          });
+          response.set('Access-Control-Allow-Origin', '*');
+          response.send(episodeArr)
+        }
+      });
+});
+
+app.use(cors())
+//get Description for an for anime endpoint
+app.get('/downloadLink', (request, response) => {
+
+    quest(`https://www1.gogoanime.ai/${request.query.link}`, (error, _response, html) => {
+    if (!error && _response.statusCode == 200) {
+          const $ = cheerio.load(html);
+          const Dlink = $('li.dowloads > a').attr('href');
+          DownloadLink(Dlink).then((data)=>{
+            response.set('Access-Control-Allow-Origin', '*');
+            response.send(data)
+        }).catch(console.error);
+
+        }
+      });
+});
+
+
+function DownloadLink (link) {
+    return new Promise((resolve, reject) => {
+        try {
+
+            quest(link, (error, _response, html) => {
+                if (!error && _response.statusCode == 200) {
+                    const $ = cheerio.load(html);
+                    const DlinkTypes =[]
+                    $('.dowload>a').each((i,el) => {
+                        if(i < 12){
+                            const title = $(el).text()
+                            const link = $(el).attr('href');
+                            DlinkTypes.push({name:title, link:link})
+                        }
+
+                    })
+
+                    return resolve(DlinkTypes);
+                }
+            })
+        } catch (e) {
+            return reject(e);
+        }
+    })
+}
+
+
+
+// setInterval(function() {
+//   quest("https://anime-web-scraper.herokuapp.com/popular/?page=1");
+// }, 300000); // every 5 minutes (300000)
